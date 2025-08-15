85704be36d153412b35c205128a17d8ee09c51cd
Herton Vilarim <hertonk@gmail.com>
2025-08-15 12:00:19 -0300

    feat: home, ad register, search results, property details

--- front/views.py
+++ front/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'front/home.html')
+
+def search_results(request):
+    return render(request, 'front/search_results.html')
     
+def property_details(request):
+    return render(request, 'front/property_details.html')
+
+def ad_register(request):
+    return render(request, 'front/ad_register.html')
+
+def login(request):
+    return render(request, 'front/login.html')
